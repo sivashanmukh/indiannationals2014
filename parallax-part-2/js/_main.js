284a8b20d5ac67b774a580832ac024c332fc036e
Siva Shanmukh Vetcha <sshanmukh@kiwiup.com>
2014-04-24 16:38:49 +0530

    Changing intermediate pics, remove sidenav

--- parallax-part-2/js/_main.js
+++ parallax-part-2/js/_main.js
@@ -77,7 +77,7 @@
 		
 		// Build HTML for Nav
 		$('<div/>', {
-		    'id' : 'slideNav',
+		    'id' : 'slideNav_',
                     'style' : 'display:none;'
 		}).append($('<ul><li class="slideNavPrev"><a class="disabled" href="#" title="Go to previous slide"><span class="ico ico-up">↑</span></a></li><li><span id="activeSlide">'+activeSlide+'</span>/<span id="maxSlides">'+slidesCount+'</span></li><li class="slideNavNext"><a href="#" title="Go to next slide"><span class="ico ico-down">↓</span></a></li></ul>')).appendTo('body').delay(1200).fadeIn(duration);
 		
